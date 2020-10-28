baef6c077ad7cbd7b05a469008f2e1aa213ea57d
peterwilfride <cybermind545@gmail.com>
2020-10-27 21:23:51 -0300

    adicionado o server

---
+++ HTTPserver.py
@@ -0,0 +1,63 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
+# DEPARTAMENTO DE ENGENHARIA DE COMPUTACAO E AUTOMACAO
+# DISCIPLINA REDES DE COMPUTADORES (DCA0113)
+# AUTOR: PROF. CARLOS M D VIEGAS (viegas 'at' dca.ufrn.br)
+#
+# SCRIPT: Base de um servidor HTTP (python 3)
+#
+
+# importacao das bibliotecas
+import socket
+
+# definicao do host e da porta do servidor
+HOST = '' # ip do servidor (em branco)
+PORT = 8080 # porta do servidor
+
+# cria o socket com IPv4 (AF_INET) usando TCP (SOCK_STREAM)
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# permite que seja possivel reusar o endereco e porta do servidor caso seja encerrado incorretamente
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# vincula o socket com a porta (faz o "bind" do IP do servidor com a porta)
+listen_socket.bind((HOST, PORT))
+
+# "escuta" pedidos na porta do socket do servidor
+listen_socket.listen(1)
+
+# imprime que o servidor esta pronto para receber conexoes
+print ('Serving HTTP on port %s ...' % PORT)
+
+while True:
+    # aguarda por novas conexoes
+    client_connection, client_address = listen_socket.accept()
+    # o metodo .recv recebe os dados enviados por um cliente atraves do socket
+    request = client_connection.recv(1024)
+    # imprime na tela o que o cliente enviou ao servidor
+    print (request.decode('utf-8'))
+    request_splited = request.decode('utf-8').split(' ')
+    # declaracao da resposta do servidor
+    try:
+        if request_splited[1] == "/" or request_splited[1] == "/index.html":
+            request_splited[1] = "index.html"
+    except:
+        pass
+               
+    if request_splited[0] != "GET":
+        print("comando desconhecido")
+        with open("badRequest.html", 'r') as file:
+             http_response = file.read()
+    else:
+        try:
+            with open(request_splited[1], 'r') as file:
+                http_response = file.read()
+        except:
+            with open("notFound.html", 'r') as file:
+                http_response = file.read() 
+    # servidor retorna o que foi solicitado pelo cliente (neste caso a resposta e generica)
+    client_connection.send(http_response.encode('utf-8'))
+    # encerra a conexao
+    client_connection.close()
+
+# encerra o socket do servidor
+listen_socket.close()
